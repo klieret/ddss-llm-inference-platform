d353ef90c8a86c89dcf0361aa08aa1450d7a9d24
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-17 05:51:39 -0400

    Wait for SLURM job to run

---
+++ src/llm_inference_platform/slurm.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import enum
+import subprocess
+import time
+from enum import auto
+from typing import Literal, Self
+
+from utils.log import logger
+
+
+class JobState(enum.Enum):
+    """Slightly coarser categorization of SLURM job states."""
+
+    RUNNING = auto()
+    COMPLETED = auto()
+    PENDING = auto()
+    FAILED = auto()
+    UNKNOWN = auto()
+
+    def from_status_str(status: str) -> Self:
+        """Interpret output of SLURM job status command"""
+        match status:
+            case "RUNNING":
+                return JobState.RUNNING
+            case "COMPLETED" | "DEADLINE":
+                return JobState.COMPLETED
+            case "PENDING" | "REQUEUED":
+                return JobState.PENDING
+            case "FAILED" | "BOOT_FAIL" | "CANCELLED" | "NODE_FAIL" | "OUT_OF_MEMORY" | "PREEMPTED" | "TIMEOUT":
+                return JobState.FAILED
+            case _:
+                return JobState.UNKNOWN
+
+
+def get_slurm_job_status(job_id: str) -> tuple[str, JobState]:
+    """Get the status of a job on the SLURM cluster.
+
+    Args:
+        job_id: The job ID of the job to get the status of.
+
+    Returns:
+        The status of the job as a string and as a `JobState` enum.
+    """
+    cmd = ["sacct", "-n", "-j", job_id, "--format=State", "-P"]
+    logger.debug("Getting SLURM job status with '%s'", " ".join(cmd))
+    status_str_full = subprocess.check_output(
+        cmd,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    )
+    logger.debug("Got status string '%s'", status_str_full)
+    status_str = status_str_full.splitlines()[0].strip()
+    return status_str, JobState.from_status_str(status_str)
+
+
+def get_slurm_start_time(job_id: str) -> str:
+    """Get the start time of a job on the SLURM cluster.
+
+    Args:
+        job_id: The job ID of the job to get the start time of.
+
+    Returns:
+        The start time of the job as a UNIX timestamp.
+    """
+    cmd = ["sacct", "-n", "-j", job_id, "--format=Start", "-P"]
+    logger.debug("Getting SLURM start time with '%s'", " ".join(cmd))
+    start_time_str_full = subprocess.check_output(
+        cmd,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    )
+    logger.debug("Got status string '%s'", start_time_str_full)
+    return start_time_str_full.splitlines()[0].strip()
+
+
+class WaitTillRunning:
+    def __init__(self, job_id: str, *, poll_interval: int = 10):
+        self._job_id = job_id
+        self._poll_interval = poll_interval
+
+    def user_feedback(
+        self, message: str, *, level: Literal["info", "error"] = "info"
+    ) -> None:
+        if level == "info":
+            logger.info(message)
+        elif level == "error":
+            logger.error(message)
+        else:
+            msg = f"Unknown level {level}"
+            raise ValueError(msg)
+
+    def wait(self) -> None:
+        self.user_feedback("Waiting for job %s to start...", self._job_id)
+        while True:
+            status_str, status = get_slurm_job_status(sys.argv[1])
+            logger.debug("Status: %s, %s", status_str, status)
+            match status:
+                case JobState.RUNNING:
+                    self.user_feedback(f"Job {self._job_id} is running.")
+                    return True
+                case JobState.PENDING:
+                    start_time = get_slurm_start_time(sys.argv[1])
+                    self.user_feedback(
+                        f"Job {self._job_id} is pending. Estimated start time {start_time}."
+                    )
+                    time.sleep(self._poll_interval)
+                case JobState.FAILED:
+                    self.user_feedback(f"Job {self._job_id} failed.", level="error")
+                    return False
+                case JobState.COMPLETED:
+                    self.user_feedback(
+                        f"Job {self._job_id} already completed. Please start a new one"
+                    )
+                    return False
+                case JobState.UNKNOWN:
+                    self.user_feedback(
+                        f"Job {self._job_id} status unknown. Please report this.",
+                        level="error",
+                    )
+                    return False
+
+
+if __name__ == "__main__":
+    import sys
+
+    wtr = WaitTillRunning(sys.argv[1])
+    wtr.wait()
---
+++ src/llm_inference_platform/utils/log.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+
+import colorlog
+
+LOG_DEFAULT_LEVEL = logging.DEBUG
+
+
+def get_logger(name="llmip", level=LOG_DEFAULT_LEVEL):
+    """Sets up global logger."""
+    _log = colorlog.getLogger(name)
+
+    if _log.handlers:
+        # the logger already has handlers attached to it, even though
+        # we didn't add it ==> logging.get_logger got us an existing
+        # logger ==> we don't need to do anything
+        return _log
+
+    _log.setLevel(level)
+
+    sh = colorlog.StreamHandler()
+    log_colors = {
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red",
+    }
+    #  This is not the same as just setting name="" in the fct arguments.
+    #  This would set the root logger to debug mode, which for example causes
+    #  the matplotlib font manager (which uses the root logger) to throw lots of
+    #  messages. Here, we want to keep our named logger, but just drop the
+    #  name.
+    name_incl = "" if name == "gnn-tracking" else f" {name}"
+    formatter = colorlog.ColoredFormatter(
+        f"%(log_color)s[%(asctime)s{name_incl}] %(levelname)s: %(message)s",
+        log_colors=log_colors,
+        datefmt="%H:%M:%S",
+    )
+    sh.setFormatter(formatter)
+    # Controlled by overall logger level
+    sh.setLevel(logging.DEBUG)
+
+    _log.addHandler(sh)
+
+    return _log
+
+
+logger = get_logger()
