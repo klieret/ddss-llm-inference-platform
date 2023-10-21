54ccac166fd8481691e43b705b6fe779d77f03b0
Kilian Lieret <kilian.lieret@posteo.de>
2023-10-21 11:29:17 +0200

    More tests for deploy.py

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,6 +1,11 @@
 import shlex
+from pathlib import Path
 
-from llm_inference_platform.deploy import format_slurm_submission_script
+from llm_inference_platform.deploy import (
+    PersistInfo,
+    construct_singularity_cmd,
+    format_slurm_submission_script,
+)
 
 
 def test_format_slurm_submission_script():
@@ -11,3 +16,23 @@ def test_format_slurm_submission_script():
     assert email in script
     assert shlex.join(cmd) in script
     assert "{{" not in script
+
+
+def test_construct_singularity_cmd():
+    sc = shlex.join(
+        construct_singularity_cmd(
+            model_name="test_model",
+            revision="1",
+        )
+    )
+    assert "--quantization" not in sc
+    assert "--port=8000" in sc
+    assert "test_model/snapshots/1" in sc
+
+
+def test_persist_info(tmp_path: Path):
+    pi = PersistInfo("123", "8000", "della-l01g02")
+    tp = tmp_path / "test.json"
+    pi.dump(tp)
+    pi_recovered = PersistInfo.from_file(tp)
+    assert pi == pi_recovered
